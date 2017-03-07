5dfd70b1804d25b29f11754a8eb89ad236de1ab4
ASBOWalrus <joemarvin95@gmail.com>
2017-03-07 12:20:10 +0000

    Testing contact form php for bombshells

---
+++ send_form_email.php
@@ -0,0 +1,72 @@
+<?php
+	if(isset($_POST['email']))
+	{
+		$email_to = "joemarvin95@gmail.com";
+		$email_subject = "Bombshells Beauty Contact Form Message";
+
+		function died($error) {
+			echo "Error:<br>";
+			echo $error;
+			die();
+		}
+
+		$email_from = $_POST['email'];
+		$name = $_POST['name'];
+		$message = $_POST['message'];
+
+		$email_message = "Form details below:\n\n";
+
+		function cleanString($string) {
+			$bad = array("content-type", "bcc:", "to:", "cc:", "href");
+			return str_replace($bad, "", $string);
+		}
+
+		$email_message .= "Name: ".cleanString($name)."\n";
+		$email_message .= "Email: ".cleanString($email_from)."\n";
+		$email_message .= "Message: ".cleanString($message)."\n";
+
+		$headers = 'From: '.$email_from."\r\n".
+		'Reply-To: '.$email_from."\r\n".
+		'X-Mailer: PHP/'.phpversion();
+
+		@mail($email_to, $email_subject, $email_message, $headers);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Contact</title>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
+    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>
+  </head>
+  <body>
+    <main class="container-fluid">
+      <div id="sent">
+				<div class="container">
+					<p class="sent-msg">
+						Message sent
+					</p>
+					<a href="index.html">Return to homepage</a>
+				</div>
+			</div>
+    </main>
+    <footer class="container-fluid">
+      <div class="container">
+        <div class="row">
+          <ul class="col-xs-12" id="social-list">
+            <li>
+              <a target="_blank" href="https://www.facebook.com/bombshellshorncastle">
+                <img src="../images/fb-logo.png" alt="Facebook">
+              </a>
+            </li>
+          </ul> 
+        </div>
+      </div>
+    </footer>
+  </body>
+</html>
+
+<?php
+	}
+?>
